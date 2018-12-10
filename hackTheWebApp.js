d128148e66a533d56f21b5a724652d30b00fb9b8
George Panagiotou <georgepanangiotou@gmail.com>
2018-12-10 17:29:36 +0200

    Update hackTheWebApp.js

--- hackTheWebApp.js
+++ hackTheWebApp.js
@@ -1,1 +1,5 @@
-document.getElementsByClassName('sc-cMljjf bqDeCs')[0].style.visibility = 'hidden';
+var appBanners = document.getElementsByClassName('sc-cMljjf bqDeCs'), i;
+
+for (var i = 0; i < appBanners.length; i ++) {
+    appBanners[i].style.display = 'none';
+}

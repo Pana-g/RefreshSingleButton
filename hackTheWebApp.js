915463ba2af13a7ca8bbafceb9e838915da918a2
George Panagiotou <georgepanangiotou@gmail.com>
2018-12-10 17:53:35 +0200

    Update hackTheWebApp.js

--- hackTheWebApp.js
+++ hackTheWebApp.js
@@ -1,8 +1,8 @@
 
  setInterval(function() {
    console.log("starts");
-   if ($('.bqDeCs').length) {
-      var appBanners = document.getElementsByClassName('bqDeCs'), i;
+   if ($('div.sc-cMljjf.bqDeCs').length) {
+      var appBanners = document.getElementsByClassName('sc-cMljjf bqDeCs'), i;
 console.log("exists");
 for (var i = 0; i < appBanners.length; i ++) {
     appBanners[i].style.display = 'none';

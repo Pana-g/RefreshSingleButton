c3f15d11d7bfcca8fe97b91a1fe82eaa9047d8b1
George Panagiotou <georgepanangiotou@gmail.com>
2018-12-10 17:40:53 +0200

    Update hackTheWebApp.js

--- hackTheWebApp.js
+++ hackTheWebApp.js
@@ -1,7 +1,8 @@
 var checkExist = setInterval(function() {
+   console.log("starts");
    if ($('#the-canvas').length) {
       var appBanners = document.getElementsByClassName('bqDeCs'), i;
-
+console.log("exists");
 for (var i = 0; i < appBanners.length; i ++) {
     appBanners[i].style.display = 'none';
 }

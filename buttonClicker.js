2a6dae1393146d877a2ad321c132b79b17b05589
George Panagiotou <georgepanangiotou@gmail.com>
2018-12-12 14:50:26 +0200

    Update buttonClicker.js

--- buttonClicker.js
+++ buttonClicker.js
@@ -1,9 +1,11 @@
 const checkInterval = 3*1000
 console.info("Button listener started.");
 function timerFunc() {
+  if (document.URL.indexOf("seniors") >= 0) {
   var btn = document.getElementsByClassName("sc-jwKygS bPNJDy");
   if (btn.length > 0) {
     btn[0].click()
+  }
   }
 }
 
